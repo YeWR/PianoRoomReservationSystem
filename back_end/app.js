81c1e0f752cc26cf78bfeb84362eb98d1ab212bd
zzh428 <624777990@qq.com>
2018-10-31 23:04:09 +0800

    routers work

--- back_end/app.js
+++ back_end/app.js
@@ -3,6 +3,7 @@ const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const views = require("./views/views")
 app.use(bodyParser());
-app.use(views.routes()).use(views.allowedMethods())
+app.use(views.routes()).use(views.allowedMethods());
 
-app.listen(3000)
+app.listen(3000);
+console.log("listen on 3000");
--- back_end/views/views.js
+++ back_end/views/views.js
@@ -1,12 +1,15 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
 
-const register_out_school = require('./register/register_out_school');
-const login_in_school = require('./login/login_in_school');
-const login_out_school = require('./login/login_out_school');
+const register_out_school = require("./register/register_out_school");
+//const login_in_school = require("./login/login_in_school");
+const login_out_school = require("./login/login_out_school");
+const order = require("./order/order");
 
-router.use('/register_out_school', register_out_school.routes(), register_out_school.allowedMethods());
-router.use('/login/login_in_school', login_in_school.routes(), login_in_school.allowedMethods());
-router.use('/login/login_out_school', login_out_school.routes(), login_out_school.allowedMethods());
+
+router.use("/register_out_school", register_out_school.routes(), register_out_school.allowedMethods());
+//router.use("/login/login_in_school", login_in_school.routes(), login_in_school.allowedMethods());
+router.use("/login/login_out_school", login_out_school.routes(), login_out_school.allowedMethods());
+router.use("/order",order.routes(),order.allowedMethods());
 
 module.exports = router;
