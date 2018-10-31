49b45be1b9874a66dac9ae0c67a527c11b3b5437
zzh428 <624777990@qq.com>
2018-10-31 11:18:15 +0800

    back_end initial commit

---
+++ back_end/app.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+const bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
+
+router.post('/login', async (ctx, next) => {
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+app.listen(3000)
