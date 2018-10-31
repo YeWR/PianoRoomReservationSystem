1f2ca5e5db9b94f13221853c54571a0edc5f0f01
zzh428 <624777990@qq.com>
2018-10-31 13:14:28 +0800

    add out school login and register

--- back_end/app.js
+++ back_end/app.js
@@ -1,19 +1,8 @@
 const Koa = require('koa');
 const app = new Koa();
-const Router = require('koa-router');
-const router = new Router();
 const bodyParser = require('koa-bodyparser');
+const views = require("./views/views")
 app.use(bodyParser());
+app.use(views.routes()).use(views.allowedMethods())
 
-router.post('/login', async (ctx, next) => {
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
 app.listen(3000)
---
+++ back_end/views/login/login_out_school.js
@@ -0,0 +1,16 @@
+const Router = require('koa-router');
+const router = new Router();
+
+router.post('/', async (ctx, next) => {
+    let username = ctx.request.body.username,
+        password = ctx.request.body.password;
+    //todo: 数据库,加密
+    console.log(`login with name: ${username}, password: ${password}`);
+    if (username === 'koa' && password === '12345') {
+        ctx.response.body = {success:true};
+    } else {
+        ctx.response.body = {success:false};
+    }
+});
+
+module.exports = router;
