b720d33f287ab41e54ee7d817ae6675a3c46d374
zzh428 <624777990@qq.com>
2018-11-09 23:38:58 +0800

    add await and fix bug

--- back_end/views/user/login_out_school.js
+++ back_end/views/user/login_out_school.js
@@ -6,8 +6,8 @@ const routers = router.post("/", async (ctx, next) => {
     let username = ctx.request.body.username,
         password = ctx.request.body.password;
     console.log(`login with name: ${username}, password: ${password}`);
-    let result = dataBase.SocietyLogin(username,password);
-    result.then(res=>{ctx.response.body = res}).catch(res=>{ctx.response.body = {success:false, info: "Database Error"}})
+    let result = await dataBase.SocietyLogin(username,password);
+    ctx.response.body = result;
 });
 
 module.exports = routers;
