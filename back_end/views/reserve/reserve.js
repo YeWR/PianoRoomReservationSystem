29d4ee0a97fcb17192c6f9f6f87e3ec99e008791
zzh428 <624777990@qq.com>
2018-11-11 00:51:56 +0800

    add reserve routers

--- back_end/views/reserve/reserve.js
+++ back_end/views/reserve/reserve.js
@@ -3,9 +3,26 @@ const router = new Router();
 const dataBase = require("../dataBase")
 
 const routers = router.get("/all", async (ctx, next) => {
-    let result = await dataBase.GetPianoRoomInfo(pianoId);
+    let result = await dataBase.GetPianoRoomAll();
+    if(result.data === null)
+    {
+        ctx.response.body = {"pianoList": null, "errorMsg":result.info};
+    }
+    else
+    {
+        let pianolist = [];
+        for(let p in result.data) {
+            let info = {
+                "pianoId": p.piano_id,
+                "pianoType": p.piano_type,
+                "timeTable": p.piano_list,
+                "pianoPlace": p.piano_room
+            };
+            pianolist.append(info);
+        }
+        ctx.response.body = {"pianoList": pianolist};
+    }
     console.log(result);
-    ctx.response.body = result;
 
 }).get("/detail", async (ctx, next) => {
     let pianoId = ctx.request.body.pianoId;
@@ -14,8 +31,20 @@ const routers = router.get("/all", async (ctx, next) => {
     {
         ctx.response.body = {"tableTime": null, "pianoPrices": null, "pianoInfo": null, "errorMsg":result.info};
     }
+    else {
+        let price = {
+            "student": result.data.piano_stuvalue,
+            "teacher": result.data.piano_teavalue,
+            "society": result.data.piano_socvalue,
+            "multi": result.data.piano_multivalue
+        };
+        ctx.response.body = {
+            "timeTable": result.data.piano_list,
+            "pianoPrices": price,
+            "pianoInfo": result.data.piano_info
+        };
+    }
     console.log(result);
-    ctx.response.body = result;
 });
 
 module.exports = routers;
