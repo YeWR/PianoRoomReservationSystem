78afc859f770dc8826565a59fb22ce47d40a7a28
zzh428 <624777990@qq.com>
2018-11-12 20:48:16 +0800

    bug fix

--- back_end/app.js
+++ back_end/app.js
@@ -15,11 +15,11 @@ app.use(session({
 app.use(async (ctx, next) => {
     let reg = /^((\/register)|(\/login)|(\/validate))/
     if(!ctx.cookies.get('koa:sess') && !reg.test(ctx.path)){
-        console.log(ctx.cookies.get('koa:sess'),ctx.path)
-        await ctx.response.body = { "success": false, "info": "请登录"};
+        console.log(ctx.cookies.get('koa:sess'),ctx.path);
+        ctx.response.body = {"success": false, "info": "请登录"};
         return;
     }
-    next()
+    next();
 })
 
 app.use(bodyParser());
