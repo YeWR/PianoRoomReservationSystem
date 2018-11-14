d6dc65a9c81620de66fbd2b547371b71bb0c3f44
zzh428 <624777990@qq.com>
2018-11-14 20:28:16 +0800

    add alarm filter

--- back_end/views/reserve/alarm.js
+++ back_end/views/reserve/alarm.js
@@ -50,6 +50,20 @@ function getDateStr (date) {
     return dateStr;
 }
 
+function compTime(nowdate, date, endIndex)
+{
+    date.setHours(8+Math.floor(endIndex/6),10*(endIndex%6),0);
+    console.log("nowdate ", nowdate, "date ", date);
+    if(date.getTime() - nowdate.getTime() > 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 const routers = router.post("/all", async (ctx, next) => {
     let userId = ctx.request.body.number;
     let result = await dataBase.GetItem(userId);
@@ -61,28 +75,32 @@ const routers = router.post("/all", async (ctx, next) => {
     {
         let reservationList = [];
         let pianoInfo = await dataBase.GetPianoRoomAll();
+        let nowDate = new Date();
         for(let p of result.data) {
             for(let i of pianoInfo.data)
             {
                 if(i.piano_id === p.item_roomId)
                 {
                     let date = new Date(p.item_date);
-                    let dateStr = getDateStr(date);
-                    let week = date.getDay();
-                    const weekStr = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
-                    let info = {
-                        "pianoPlace":i.piano_room,
-                        "pianoType": i.piano_type,
-                        "pianoPrice": p.item_value,
-                        "reservationType": p.item_member,
-                        "reservationState": p.item_type,
-                        "reservationId": p.item_id,
-                        "date": dateStr,
-                        "weekday": weekStr[week],
-                        "begTimeIndex": p.item_begin,
-                        "endTimeIndex": p.item_begin+p.item_duration
-                    };
-                    reservationList.push(info);
+                    if(compTime(nowDate, date, p.item_begin+p.item_duration))
+                    {
+                        let dateStr = getDateStr(date);
+                        let week = date.getDay();
+                        const weekStr = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+                        let info = {
+                            "pianoPlace": i.piano_room,
+                            "pianoType": i.piano_type,
+                            "pianoPrice": p.item_value,
+                            "reservationType": p.item_member,
+                            "reservationState": p.item_type,
+                            "reservationId": p.item_id,
+                            "date": dateStr,
+                            "weekday": weekStr[week],
+                            "begTimeIndex": p.item_begin,
+                            "endTimeIndex": p.item_begin + p.item_duration
+                        };
+                        reservationList.push(info);
+                    }
                     break;
                 }
             }
