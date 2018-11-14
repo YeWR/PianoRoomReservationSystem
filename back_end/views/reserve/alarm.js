94a259dc655585bf20bf112d8e56f600a151f219
zzh428 <624777990@qq.com>
2018-11-14 10:22:37 +0800

    edit date format

--- back_end/views/reserve/alarm.js
+++ back_end/views/reserve/alarm.js
@@ -18,13 +18,16 @@ const routers = router.post("/all", async (ctx, next) => {
             {
                 if(i.piano_id === p.item_roomId)
                 {
+                    let date = new Date(p.item_date);
+                    console.log(date.toISOString());
+                    let dateStr = date.getFullYear().toString() + "年" + date.getMonth().toString() + "月" + date.getDate().toString() + "日";
                     let info = {
                         "pianoPlace":i.pianoPlace,
                         "pianoType": i.pianoType,
                         "pianoPrice": p.item_value,
                         "reservationType": p.item_member,
                         "reservationState": p.item_type,
-                        "date": p.item_date,
+                        "date": dateStr,
                         "begTimeIndex": p.item_begin,
                         "endTimeIndex": p.item_begin+p.item_duration
                     };
