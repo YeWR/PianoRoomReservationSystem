dcc38b6df123f0eb57cdafef051fbf18e7a22413
zzh428 <624777990@qq.com>
2018-11-14 19:40:42 +0800

    fix date format

--- back_end/views/reserve/alarm.js
+++ back_end/views/reserve/alarm.js
@@ -27,6 +27,30 @@ function sortItem(a, b)
     }
 }
 
+function getDateStr (date) {
+    let dateStr = date.getFullYear().toString() + "年";
+    let month = date.getMonth()+1;
+    String.format()
+    let day = date.getDate();
+    if(month < 10)
+    {
+        dateStr = dateStr + "0" + month.toString() + "月";
+    }
+    else
+    {
+        dateStr = dateStr + month.toString() + "月";
+    }
+    if(day < 10)
+    {
+        dateStr = dateStr + "0" + day.toString() + "日";
+    }
+    else
+    {
+        dateStr = dateStr + day.toString() + "日";
+    }
+    return dateStr;
+}
+
 const routers = router.post("/all", async (ctx, next) => {
     let userId = ctx.request.body.number;
     let result = await dataBase.GetItem(userId);
@@ -44,8 +68,7 @@ const routers = router.post("/all", async (ctx, next) => {
                 if(i.piano_id === p.item_roomId)
                 {
                     let date = new Date(p.item_date);
-                    console.log(date.toISOString());
-                    let dateStr = date.getFullYear().toString() + "年" + (date.getMonth()+1).toString() + "月" + date.getDate().toString() + "日";
+                    let dateStr = getDateStr(date);
                     let week = date.getDay();
                     const weekStr = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
                     let info = {
