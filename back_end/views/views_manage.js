d46a35b4d3d23c8a230c398ef654038a0a6df20c
zzh428 <624777990@qq.com>
2018-12-03 23:56:02 +0800

    add logout

--- back_end/views/views_manage.js
+++ back_end/views/views_manage.js
@@ -3,8 +3,10 @@ const router = new Router();
 
 const manage_login = require("./manage/login");
 const manage_info = require("./manage/info");
+const manage_logout = require("./manage/logout");
 
 router.use("/manager/login", manage_login.routes(), manage_login.allowedMethods());
 router.use("/manager/info", manage_info.routes(), manage_info.allowedMethods());
+router.use("/manager/logout", manage_logout.routes(), manage_logout.allowedMethods());
 
 module.exports = router;
