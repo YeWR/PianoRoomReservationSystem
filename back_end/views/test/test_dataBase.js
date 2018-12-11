d5370dfaab4b7fe3629fc402787d70fd8e80f2a1
zzh428 <624777990@qq.com>
2018-12-11 23:35:39 +0800

    Update test_dataBase.js

--- back_end/views/test/test_dataBase.js
+++ back_end/views/test/test_dataBase.js
@@ -7,7 +7,8 @@ const request = supertest(app.listen());
 describe('#dataBase',()=>{
     describe('GetSocietyUuidByTele',()=>{
         it('should return 5 when 2 + 3',() =>{
-            let result = await dataBase.GetSocietyUuidByTele("13220167398");
+            console.log(dataBase);
+            let result = dataBase.GetSocietyUuidByTele("13220167398");
             expect(result.success).to.have.property('data');
         });
     })
