ce09d1eeacf5af3ef71bc2d337c1f766fe7d6256
zzh428 <624777990@qq.com>
2019-01-05 19:06:48 +0800

    删除无用文件&无用包引用

--- back_end/app.js
+++ back_end/app.js
@@ -4,20 +4,15 @@ const xmlParser = require('koa-xml-body');
 const bodyParser = require("koa-bodyparser");
 const views = require("./views/views");
 const views_manage = require("./views/views_manage");
-const session = require("koa-session");
 const cors = require("koa2-cors");
 let file = require("fs");
 let dbconfigFile = "mysqlConfig.json";
 let dbconfig = JSON.parse(file.readFileSync(dbconfigFile));
 const configPath = "configs.json";
 const configs = JSON.parse(file.readFileSync(configPath));
-const stat = require('koa-static');
 
 
 app.keys = configs.app_key;
-app.use(session({
-    maxAge: configs.expire_day *24*60*60*1000,
-},app));
 
 app.use(cors());
 app.use(xmlParser({
@@ -27,13 +22,13 @@ app.use(xmlParser({
         normalizeTags: true, // Transform tags to lowercase
         explicitArray: false // Only put nodes in array if >1
     }
-}))
+}));
 app.use(bodyParser());
 
 app.use(views.routes()).use(views.allowedMethods());
 
 app.use(views_manage.routes()).use(views_manage.allowedMethods());
-app.use(stat(__dirname));
+
 if(!module.parent)
 {
     app.listen(3000);
